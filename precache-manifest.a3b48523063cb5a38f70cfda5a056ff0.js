82e322b2a4090160575ca27d576ffd9d645e9376
Miguel Galarraga <miguelangelgalarraga@gmail.com>
2020-02-25 16:49:26 -0500

    Updates

---
+++ precache-manifest.a3b48523063cb5a38f70cfda5a056ff0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed7bba4650fef4a2688ee59eaec0a0cf",
+    "url": "/clickgame/index.html"
+  },
+  {
+    "revision": "b25967361a77a9b615fc",
+    "url": "/clickgame/static/css/main.94bebd66.chunk.css"
+  },
+  {
+    "revision": "2c1b24d78168709ae7ac",
+    "url": "/clickgame/static/js/2.a4a2581f.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/clickgame/static/js/2.a4a2581f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b25967361a77a9b615fc",
+    "url": "/clickgame/static/js/main.7e202f3b.chunk.js"
+  },
+  {
+    "revision": "142cc84a105f54549d1b",
+    "url": "/clickgame/static/js/runtime-main.0cac328c.js"
+  }
+]);
